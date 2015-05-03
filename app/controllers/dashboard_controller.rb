89feadcd948ceadbf765477c80e711aa68a646e0
jiazhen <fdf515@gmail.com>
2015-05-03 12:08:49 +0100

    Hide credentials to yml

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @github_client_id = APP_CONFIG["github_client_id"]
+    @github_client_secret = APP_CONFIG["github_client_secret"]
   end
 end
