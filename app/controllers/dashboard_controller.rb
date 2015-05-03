b38198d3f7ba2100b468f3a763e21fb5112a8cf4
jiazhen <fdf515@gmail.com>
2015-05-03 17:21:38 +0100

    Use github_api for authentication

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
   def index
-    @github_client_id = APP_CONFIG["github_client_id"]
-    @github_client_secret = APP_CONFIG["github_client_secret"]
   end
 end
---
+++ app/helpers/github_helper.rb
@@ -0,0 +1,16 @@
+module GithubHelper
+
+  def scopes
+    "user:email,repo".freeze
+  end
+
+  def github
+    @github ||= Github.new(client_id: APP_CONFIG["github_client_id"],
+      client_secret: APP_CONFIG["github_client_secret"])
+  end
+
+  def authorize_url
+    github.authorize_url(scope: scopes)
+  end
+
+end
